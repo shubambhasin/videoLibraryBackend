8d3e05d92bbd49ea1a56ddc626b2afbd7533cc00
Shubam Bhasin <>
2021-04-29 14:23:46 +0000

    data POST req added

--- index.js
+++ index.js
@@ -1,10 +1,19 @@
 const express = require('express');
 const app = express();
+const cors = require('cors')
+const mongoose = require('mongoose')
 const router = require('./routers/videosRouter.js')
 const connectDatabase = require("./DB/db.connect.js")
 
 
+app.use(cors())
+app.use(express.json());
 connectDatabase()
+mongoose.set('useNewUrlParser', true);
+mongoose.set('useFindAndModify', false);
+mongoose.set('useCreateIndex', true);
+
+
 app.get('/', (req, res) => {
   res.send('careTv API')
 });
--- routers/videosRouter.js
+++ routers/videosRouter.js
@@ -1,12 +1,36 @@
 const express = require("express")
 const router = express.Router()
-
+const { Video } = require('../models/videosModel.js')
 router.route('/')
-.get((req, res) => {
-  res.send("Videos showm here")
+.get( async (req, res) => {
+
+  const videos = await Video.find() 
+  res.status(200).json({ success: true, data: videos })
 })
-.post((req, res) => {
-  res.json(req.body)
+.post( async (req, res) => {
+
+   try {
+    const {name, date, thumbnail, url, duration, description, category, videoId, subCategory} = req.body
+    const video = new Video( {
+      name: name, 
+      date: date, 
+      thumbnail: thumbnail, 
+      duration: duration, 
+      description: description, 
+      url: url,
+      category: category,
+      videoId: videoId,
+      subCategory: subCategory
+
+    })
+    const savedVideo = await video.save()
+    console.log(savedVideo)
+    res.json({success: true, data: savedVideo})
+       } 
+   catch(err){
+        res.json({success: false, errorrrrr: err})
+    }
+
 })
 
 module.exports = router;
